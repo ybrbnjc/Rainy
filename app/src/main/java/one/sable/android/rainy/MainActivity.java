bc51c73450d63e81677549348476202f38431677
SableOne <n.voronov@bk.ru>
2018-10-01 22:38:32 +0300

    first step on interface

--- app/src/main/java/one/sable/android/rainy/MainActivity.java
+++ app/src/main/java/one/sable/android/rainy/MainActivity.java
@@ -1,7 +1,12 @@
 package one.sable.android.rainy;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // TODO: Implement this method
+        getMenuInflater().inflate(R.layout.menu_activity_main, menu);
+        return true;
+    }
+    /**
+     * A simple {@link Fragment} subclass.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+
+        public PlaceholderFragment() {
+            // Required empty public constructor
+        }
+
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+
     }
 }
